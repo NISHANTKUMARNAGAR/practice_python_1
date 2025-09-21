70841821f376c91e8e3e276344754411087efed0
NISHANTKUMARNAGAR <nishantnagar26801@gmail.com>
2025-09-21 15:51:19 +0530

    decode and encode updated

---
+++ moduletesting of module2 & mymodule.py
@@ -0,0 +1,17 @@
+#import mymodule as m
+import math
+import sys
+print('printing path')
+print(sys.path)
+from module2 import w as p
+from mymodule import w as q
+def w():
+   print('function in test')
+w()
+p()
+q()
+#p()
+#print(v)
+print(dir(math))
+x=5
+print(globals())
