5fa809387ee798f8392382b85d77ec0f706ffc49
NISHANTKUMARNAGAR <nishantnagar26801@gmail.com>
2025-10-10 10:34:36 +0530

    update

--- generator function.py
+++ generator function.py
@@ -1,17 +1,20 @@
-"""def func(n):
-    c=1
-    while c<=n:
-        print('before yield')
-        yield c
-        print("after yield")
-        c=c+1
+#basic functionality of generator
+"""def func():
+    for i in range(5):
+        yield i
+    print('after loop')
 
-itera=func(5)
-print('after getting generator object')
-for i in itera:
-    print('before printing number')
-    print(i)
-    print("after printing number")"""
+p=func()
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+try:
+    print(next(p))
+except StopIteration:
+    print("stop iteration is raised")"""
+
 
 #example without loop in generator  function
 """def func():
@@ -21,6 +24,22 @@ for i in itera:
     yield 3
 
 itera=func()
+print('after getting generator object')
+for i in itera:
+    print('before printing number')
+    print(i)
+    print("after printing number")"""
+
+#generator with loop
+"""def func(n):
+    c=1
+    while c<=n:
+        print('before yield')
+        yield c
+        print("after yield")
+        c=c+1
+
+itera=func(5)
 print('after getting generator object')
 for i in itera:
     print('before printing number')
